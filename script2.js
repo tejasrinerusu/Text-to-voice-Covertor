0d68aeea530bd362b657340adafb7affb204eb00
NERUSUTEJASRI <tejasrinerusu@gmail.com>
2024-06-15 10:18:08 +0530

    Add all files

---
+++ script2.js
@@ -0,0 +1,23 @@
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voiceSelect.innerHTML = '';
+    voices.forEach((voice, i) => {
+        let option = new Option(voice.name, i);
+        voiceSelect.add(option);
+    });
+};
+
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
+});
+
+document.querySelector("button").addEventListener("click", () => {
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+});
